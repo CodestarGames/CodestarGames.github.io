315f8e7ae5471d38261ffb13184609753597f5c9
Donald Baird <donald@homerlearning.com>
2021-10-30 18:46:53 +0900

    comitting changed

---
+++ dist/assets/tilemaps/marblesTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="marblesTileset" tilewidth="16" tileheight="16" tilecount="14" columns="7">
+ <image source="marblesTileset.png" trans="003039" width="112" height="40"/>
+</tileset>
---
+++ dist/assets/tilemaps/premierTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="premierTileset" tilewidth="16" tileheight="16" tilecount="143" columns="13">
+ <image source="premierTileset.png" width="208" height="176"/>
+</tileset>
